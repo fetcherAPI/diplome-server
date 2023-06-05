cf20173f537544e3f1b87c644a3286d91b6e70a2
Zhaisan <jnarynbek@infosystems.kg>
2023-06-05 18:39:03 +0600

    try to add swagger

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import { registerValidator } from "./validations/auth.js";
 import checkToken from "./utils/checkToken.js";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 import { register, login, getMe } from "./controllers/UserController.js";
 import {
@@ -11,11 +14,48 @@ import {
 import { newVacancyValidator } from "./validations/newVacancyValidator.js";
 
 const app = express();
+
 mongoose
   .connect("mongodb+srv://root:12345@cluster0.jq6wt5c.mongodb.net/blog")
   .then(() => console.log("DB"))
   .catch((err) => console.log("error", err));
-app.use(express.json());
+
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:4444",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJSDoc(options);
+
+app.use(
+  "/api-docs",
+  express.json(),
+  cors(),
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
 
 app.post("/auth/register", registerValidator, register);
 
